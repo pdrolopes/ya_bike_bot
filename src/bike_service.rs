c1689de53f553e5c0c46692fe3147e6ee7fb8281
Pedro Lopes de Azevedo <ordepi@gmail.com>
2020-03-21 20:26:13 -0300

    Retrieving 5 closest stations

--- src/bike_service.rs
+++ src/bike_service.rs
@@ -1,11 +1,12 @@
 use serde::{Deserialize, Serialize};
-use surf;
-const NETWORKS_HREF: &str = "http://api.citybik.es/v2/networks";
+use surf::Exception;
+const CITYBIKES_HOST: &str = "http://api.citybik.es";
+const NETWORKS_HREF: &str = "/v2/networks";
 
 #[derive(Serialize, Deserialize, Debug)]
-struct Location {
-    latitude: f32,
-    longitude: f32,
+pub struct Location {
+    latitude: f64,
+    longitude: f64,
     city: String,
     country: String,
 }
@@ -21,10 +22,10 @@ pub struct Station {
     free_bikes: u32,
     empty_slots: u32,
     id: String,
-    latitude: f32,
-    longitude: f32,
-    name: String,
-    timestamp: String,
+    latitude: f64,
+    longitude: f64,
+    pub name: String,
+    timestamp: String, //TODO see how to manipulate dates
     extra: Extra,
 }
 
@@ -35,30 +36,58 @@ pub struct Extra {
     status: String,
 }
 
-type AsyncError = Box<dyn std::error::Error + Send + Sync + 'static>;
-
-pub async fn fetch_networks() -> Result<Vec<Network>, AsyncError> {
+pub async fn fetch_networks() -> Result<Vec<Network>, Exception> {
     #[derive(Deserialize, Serialize)]
     struct Response {
         networks: Vec<Network>,
     }
-    let Response { networks } = surf::get(NETWORKS_HREF).recv_json().await?;
+    let Response { networks } = surf::get(CITYBIKES_HOST.to_owned() + NETWORKS_HREF)
+        .recv_json()
+        .await?;
     Ok(networks)
 }
 
 impl Network {
-    async fn stations(&self) -> Result<Vec<Station>, AsyncError> {
+    pub async fn stations(&self) -> Result<Vec<Station>, Exception> {
         #[derive(Deserialize, Serialize)]
         struct Response {
             network: Network,
         }
-        let Response { network } = surf::get(NETWORKS_HREF).recv_json().await?;
+        let Network { href, .. } = self;
+        let Response { network } = surf::get(CITYBIKES_HOST.to_string() + &href)
+            .recv_json()
+            .await?;
         let Network { stations, .. } = network;
-        let stations = if let Some(stations) = stations {
-            stations
-        } else {
-            vec![]
-        };
+        let stations = stations.unwrap_or(vec![]);
         Ok(stations)
+    }
+}
+
+pub trait Geo {
+    fn location(&self) -> geoutils::Location;
+}
+
+impl Geo for Network {
+    fn location(&self) -> geoutils::Location {
+        let location = &self.location;
+        geoutils::Location::new(location.latitude, location.longitude)
+    }
+}
+
+impl Geo for Station {
+    fn location(&self) -> geoutils::Location {
+        geoutils::Location::new(self.latitude, self.longitude)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_networks() {
+        let result = fetch_networks().await;
+        println!("{:#?}", result);
+        assert!(result.is_ok());
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use dotenv;
 mod bike_service;
+use bike_service::{Geo, Station};
+use geoutils;
 use std::env;
+use std::f64::INFINITY;
 use teloxide::prelude::*;
-use teloxide::types::{MediaKind, MessageKind};
+use teloxide::types::{Location, MediaKind, MessageKind};
 
 #[tokio::main]
 async fn main() {
@@ -21,30 +24,66 @@ async fn run() {
         .messages_handler(|rx: DispatcherHandlerRx<Message>| {
             rx.for_each(|context| async move {
                 let message = &context.update;
-                if let MessageKind::Common { media_kind, .. } = &message.kind {
-                    if let MediaKind::Location { .. } = media_kind {
-                        handle_location_message(context).await
+                let answer_message = if let MessageKind::Common { media_kind, .. } = &message.kind {
+                    if let MediaKind::Location { location } = media_kind {
+                        build_near_stations_message(location).await
                     } else {
-                        context.answer("???").send().await.log_on_error().await
+                        "???".to_string()
                     }
-                }
+                } else {
+                    "???".to_string()
+                };
+                context
+                    .answer(answer_message)
+                    .send()
+                    .await
+                    .log_on_error()
+                    .await
             })
         })
         .dispatch()
         .await;
 }
-
-async fn handle_location_message(context: DispatcherHandlerCx<Message>) {
-    if let Ok(networks) = bike_service::fetch_networks().await {
-        let network = networks.first();
-        log::info!("{:?}", network);
-        context.answer("ok").send().await.log_on_error().await;
-    } else {
-        context
-            .answer("achei nada")
-            .send()
-            .await
-            .log_on_error()
-            .await;
+async fn build_near_stations_message(location: &Location) -> String {
+    match find_near_stations(location).await {
+        Ok(stations) => {
+            let first_five: Vec<String> = stations.iter().map(|s| s.name.clone()).take(5).collect();
+            log::debug!("{:?}", first_five);
+            format!("Found {} stations. {:?}", stations.len(), first_five)
+        }
+        Err(err) => {
+            log::error!("Error fetching stations {:?}", err);
+            "No stations found".to_string()
+        }
     }
+}
+
+type AsyncError = Box<dyn std::error::Error + Send + Sync + 'static>;
+
+async fn find_near_stations(location: &Location) -> Result<Vec<Station>, AsyncError> {
+    let user_location = geoutils::Location::new(location.latitude, location.longitude);
+    log::info!("pedro");
+    let mut networks = bike_service::fetch_networks().await?;
+    log::info!("{:?}", networks.len());
+    networks.sort_by_key(|network| {
+        user_location
+            .distance_to(&network.location())
+            .unwrap_or(geoutils::Distance::from_meters(INFINITY))
+            .meters() as u32
+    });
+    //TODO remove unwrap
+    // log::info!("{:?}", network);
+    let mut stations = if let Some(network) = networks.first() {
+        network.stations().await?
+    } else {
+        vec![]
+    };
+    log::info!("{:?}", stations.len());
+    stations.sort_by_key(|station| {
+        user_location
+            .distance_to(&station.location())
+            .unwrap_or(geoutils::Distance::from_meters(INFINITY))
+            .meters() as u32
+    });
+    Ok(stations)
 }
