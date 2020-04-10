9a82b1b144ad4519738ad5e28ca9941b562da6fd
Pedro Lopes de Azevedo <ordepi@gmail.com>
2020-04-10 16:15:26 -0300

    Clippy lint fix

--- src/main.rs
+++ src/main.rs
@@ -30,11 +30,11 @@ async fn run() {
     let token = env::var("TOKEN").expect("Missing TOKEN env");
     let host = env::var("HOST").expect("Missing HOST env");
     let poll: bool = env::var("POLL")
-        .unwrap_or("false".to_string())
+        .unwrap_or_else(|_| "false".to_string())
         .parse()
         .expect("non boolean value");
     let port: u16 = env::var("PORT")
-        .unwrap_or("3000".to_string())
+        .unwrap_or_else(|_| "3000".to_string())
         .parse()
         .expect("non interger value");
 
